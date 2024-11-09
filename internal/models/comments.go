32481e5b88cc5f28bf700a38c55b03e6cf71fab6
MazzMS <maximo.a.olivera@gmail.com>
2024-11-08 23:38:54 -0300

    separate DB models with own models

---
+++ internal/models/comments.go
@@ -0,0 +1,58 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/maxolivera/gophis-social-network/internal/database"
+)
+
+type Comment struct {
+	ID        uuid.UUID `json:"id"`
+	PostID    uuid.UUID `json:"post_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Content   string    `json:"content"`
+	User      User      `json:"user"`
+}
+
+func DBCommentToComment(dbComment database.Comment) Comment {
+	return Comment{
+		ID:        dbComment.ID.Bytes,
+		PostID:    dbComment.PostID.Bytes,
+		CreatedAt: dbComment.CreatedAt.Time,
+		UpdatedAt: dbComment.UpdatedAt.Time,
+		Content:   dbComment.Content,
+		User:      User{ID: dbComment.UserID.Bytes},
+	}
+}
+
+func DBCommentsToComment(dbComments []database.Comment) []Comment {
+	comments := make([]Comment, len(dbComments))
+	for i, dbComment := range dbComments {
+		comments[i] = DBCommentToComment(dbComment)
+	}
+	return comments
+}
+
+func DBCommentWithUser(dbComment database.GetCommentsByPostRow) Comment {
+	return Comment{
+		ID:        dbComment.ID.Bytes,
+		CreatedAt: dbComment.CreatedAt.Time,
+		Content:   dbComment.Content,
+		User: User{
+			Username:  dbComment.Username.String,
+			FirstName: dbComment.Username.String,
+			Email:     dbComment.Email.String,
+		},
+	}
+}
+
+// Maps returned query
+func DBCommentsWithUser(dbComments []database.GetCommentsByPostRow) []Comment {
+	comments := make([]Comment, len(dbComments))
+	for i, dbComment := range dbComments {
+		comments[i] = DBCommentWithUser(dbComment)
+	}
+	return comments
+}
---
+++ internal/models/users.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/maxolivera/gophis-social-network/internal/database"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+}
+
+func DBUserToUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID.Bytes,
+		CreatedAt: dbUser.CreatedAt.Time,
+		UpdatedAt: dbUser.UpdatedAt.Time,
+		Email:     dbUser.Email,
+		Username:  dbUser.Username,
+		FirstName: dbUser.FirstName.String,
+		LastName:  dbUser.LastName.String,
+	}
+}
+
+func DBUsersToUser(dbUsers []database.User) []User {
+	users := make([]User, len(dbUsers))
+	for i, dbUser := range dbUsers {
+		users[i] = DBUserToUser(dbUser)
+	}
+	return users
+}
