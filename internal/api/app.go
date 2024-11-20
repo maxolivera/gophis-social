7d45118b2afe3ccb9305cfa8462a06525ca06070
MazzMS <maximo.a.olivera@gmail.com>
2024-11-19 22:11:17 -0300

    graceful shutdown

--- internal/api/app.go
+++ internal/api/app.go
@@ -1,8 +1,13 @@
 package api
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -86,9 +91,38 @@ func (app *Application) Start() error {
 		IdleTimeout:  time.Minute,
 	}
 
-	app.Logger.Infow("server has started", "addr", app.Config.Addr, "env", app.Config.Environment)
+	// TODO(maolivera): Maybe move this to main?
 
-	return srv.ListenAndServe()
+	// == Graceful Shutdown ==
+	shutdown := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		app.Logger.Infow("signal caught", "signal", s.String())
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
+	app.Logger.Infow("server has started", "addr", app.Config.Addr, "env", app.Config.Environment)
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+
+	app.Logger.Infow("server has stopped", "addr", app.Config.Addr, "env", app.Config.Environment)
+
+	return nil
 }
 
 func (app *Application) GetHandlers() http.Handler {
